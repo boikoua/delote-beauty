fb6f8c0852b5fc673412d7e0b5d532134ee1c70b
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-11 10:42:51 +0200

    NotFound page

--- src/app/[...not-found]/page.tsx
+++ src/app/[...not-found]/page.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image';
 import styles from './page.module.scss';
 
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <main className={styles.main}>
+      <div className="container">
+        <Image
+          className={styles.img}
+          src="/images/not-found.png"
+          alt="Not Found Page"
+          width={600}
+          height={450}
+        />
+      </div>
+    </main>
+  );
 };
 
 export default NotFound;
